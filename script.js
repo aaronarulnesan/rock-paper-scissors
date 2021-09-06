4a7d9b4544d8e3b3a7ae09c2b2909a2104456c4d
Aaron Arulnesan <arulnesanaaron@gmail.com>
2021-09-06 15:41:25 -0400

    Added a very basic UI in html, need to iron some bugs regarding round logic

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//global variables
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 1;
+const WIN = 1;
+const LOSE = -1;
+const DRAW = 0;
 
 
 function computerPlay() {
@@ -12,9 +19,28 @@ function computerPlay() {
 
 }
 
+function buttonSelection(button) {
+
+    switch (button.id) {
+        case 'rockPlay':
+            return 'rock';
+            break;
+        case 'paperPlay':
+            return 'paper';
+            break;
+        case 'scissorsPlay':
+            return 'scissors';
+            break;
+        default:
+            return 'ERROR'
+    }
+
+}
 
 
 function playRound(playerSelection, computerSelection) {
+
+    const results = document.querySelector('#results');
 
     //converting selection of the player and computer choices to lower case
     let playerSelectionCleaned = playerSelection.toLowerCase();
@@ -23,96 +49,122 @@ function playRound(playerSelection, computerSelection) {
     //Outcome messages stored in arrays to save time repeating messages
     const outcome = ["Paper beats Rock!", "Rock beats Scissors!", "Scissors beats Paper!"];
     const winLose = [" You lose!", " You win!"];
-    
+
     //main logic of each round
     //if player and computer choose the same choice, the result is a draw
     if (computerSelectionCleaned === playerSelectionCleaned) {
-        alert("It's a draw! You both chose " + playerSelectionCleaned + "!");
+        results.innerText = `It's a draw! You both chose ${playerSelectionCleaned}!`;
         return DRAW;
-    } 
+    }
     // else the player's choice is compared with the computer's choice and the winner is determined
     else {
         switch (playerSelection) {
             case "rock":
                 if (computerSelectionCleaned === "paper") {
-                    alert(outcome[0] + winLose[0]);
+                    results.innerText = `${outcome[0] + winLose[0]}`;
                     return LOSE;
                 } else if (computerSelectionCleaned === "scissors") {
-                    alert(outcome[1] + winLose[1]);
+                    results.innerText = `${outcome[1] + winLose[1]}`;
                     return WIN;
                 }
                 break;
             case "paper":
                 if (computerSelectionCleaned === "scissors") {
-                    alert(outcome[2] + winLose[0]);
+                    results.innerText = `${outcome[2] + winLose[0]}`;
                     return LOSE;
                 } else if (computerSelectionCleaned === "rock") {
-                    alert(outcome[0] + winLose[1]);
+                    results.innerText = `${outcome[0] + winLose[1]}`;
                     return WIN;
                 }
                 break;
             case "scissors":
                 if (computerSelectionCleaned === "rock") {
-                    alert(outcome[1] + winLose[0]);
+                    results.innerText = `${outcome[1] + winLose[0]}`;
                     return LOSE;
                 } else if (computerSelectionCleaned === "paper") {
-                    alert(outcome[2] + winLose[1]);
+                    results.innerText = `${outcome[2] + winLose[1]}`;
                     return WIN;
 
                 }
                 break;
             default:
-                alert("Invalid Choice");
+                results.innerText = `Invalid Choice`;
+                console.log("Invalid Choice");
                 return DRAW;
         }
     }
 
 }
 
-//function for running a 5 round match
-function game() {
+
+function game(playerSelection) {
 
     let matchResult = 0;
-    let playerSelection = '';
     let outcomeMessage = '';
+    const score = document.querySelector('#score');
+    const replay = document.querySelector('#replay');
+    const results = document.querySelector('#results');
+    const playerChoices = document.querySelector('#playerChoices');
 
 
-    alert("Rock, Paper, Scissors!\n5 rounds total! Begin!");
+    console.log("Rock, Paper, Scissors!\n5 rounds total! Begin!");
 
-    //Loop which runs a round each iteration and tallies the resulting score
-    for(let i = 1; i <= 5; i++) {
-        playerSelection = prompt("Round " + i + "!" 
-        + "\nPlayer Score: " + playerScore 
-        + "\nComputer Score: " + computerScore
-        + "\nRock, paper, or scissors?");
+    if (rounds <= 5) {
+        score.innerText = `Round  ${rounds}!
+        \nPlayer Score:  ${playerScore}
+        \nComputer Score: ${computerScore}
+        \nRock, paper, or scissors?`
+
+        console.log("Round " + rounds + "!" +
+            "\nPlayer Score: " + playerScore +
+            "\nComputer Score: " + computerScore +
+            "\nRock, paper, or scissors?");
         matchResult = playRound(playerSelection, computerPlay());
-        if(matchResult === WIN ) {
+        if (matchResult === WIN) {
             playerScore++;
         } else if (matchResult === LOSE) {
             computerScore++;
-        }
-    }
-    //Determining the final outcome message
-    if(playerScore>computerScore) {
-        outcomeMessage = '\nYou Win!';
+        } 
+        rounds++;
     } else {
-        outcomeMessage = '\nYou Lose!';
+        if (playerScore > computerScore) {
+            outcomeMessage = 'You Win!';
+        } else {
+            outcomeMessage = 'You Lose!';
+        }
+
+        results.classList.toggle('block');
+
+        //Final outcome message
+        score.innerText =
+            `\nPlayer Score:  ${playerScore}
+        \nComputer Score: ${computerScore}
+        \n${outcomeMessage}`;
+
+        replay.addEventListener('click', () => {
+            playerScore = 0;
+            computerScore = 0;
+            rounds = 1;
+            results.classList.toggle('block');
+            score.innerText = '';
+            results.innerText = '';   
+        });
+
     }
 
-    //Final outcome message
-    alert("Final Scores: " 
-    + "\nPlayer: " + playerScore
-    + "\nComputer: " + computerScore
-    + outcomeMessage);
+    //Determining the final outcome message
 
 }
 
-//global variables
-let playerScore = 0;
-let computerScore = 0;
-const WIN = 1;
-const LOSE = -1;
-const DRAW = 0;
 
-//Running the game
-game();
+const playerChoices = document.querySelector('#playerChoices');
+const choiceBtns = playerChoices.querySelectorAll('button');
+let playerChoice = '';
+
+
+choiceBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        game(buttonSelection(button));
+        
+    });
+});
