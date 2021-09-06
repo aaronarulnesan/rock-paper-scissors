b94d829c1a16fd57a3a5d46f84ed4d010b91f440
Aaron Arulnesan <arulnesanaaron@gmail.com>
2021-09-06 18:46:37 -0400

    A complete refractor of abyssmal code

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-//global variables
+// GAME LOGIC
+
 let playerScore = 0;
 let computerScore = 0;
 let rounds = 1;
-const WIN = 1;
-const LOSE = -1;
-const DRAW = 0;
+let roundOutcome = '';
+let matchOutcome = '';
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundOutcome = 'tie';
+    } else if ( playerSelection === 'ROCK' && computerSelection === 'SCISSORS' ||
+                playerSelection === 'SCISSORS' && computerSelection === 'PAPER'  ||
+                playerSelection === 'PAPER' && computerSelection === 'ROCK'
+    ){
+        playerScore++;
+        roundOutcome = 'win';
+    } else {
+        computerScore++;
+        roundOutcome = 'loss';
+    }
+}
 
 function computerPlay() {
     // Array of plays (rock, paper, or scissors) for computer to randomly choose from
-    let plays = ["Rock", "Paper", "Scissors"];
+    let plays = ['ROCK', 'PAPER', 'SCISSORS'];
 
     //Random number between 0 to 2
     let randomNumber = Math.floor(Math.random() * 3);
@@ -19,152 +33,76 @@ function computerPlay() {
 
 }
 
-function buttonSelection(button) {
-
-    switch (button.id) {
-        case 'rockPlay':
-            return 'rock';
-            break;
-        case 'paperPlay':
-            return 'paper';
-            break;
-        case 'scissorsPlay':
-            return 'scissors';
-            break;
-        default:
-            return 'ERROR'
+function updateRoundOutcome() {
+    if (roundOutcome === 'tie') {
+        console.log('its a tie');
+    } else if (roundOutcome === 'win') {
+        console.log('you win');
+    } else {
+        console.log('you lost');
     }
-
+    updateScore();
 }
 
-
-function playRound(playerSelection, computerSelection) {
-
-    const results = document.querySelector('#results');
-
-    //converting selection of the player and computer choices to lower case
-    let playerSelectionCleaned = playerSelection.toLowerCase();
-    let computerSelectionCleaned = computerSelection.toLowerCase();
-
-    //Outcome messages stored in arrays to save time repeating messages
-    const outcome = ["Paper beats Rock!", "Rock beats Scissors!", "Scissors beats Paper!"];
-    const winLose = [" You lose!", " You win!"];
-
-    //main logic of each round
-    //if player and computer choose the same choice, the result is a draw
-    if (computerSelectionCleaned === playerSelectionCleaned) {
-        results.innerText = `It's a draw! You both chose ${playerSelectionCleaned}!`;
-        return DRAW;
+function updateMatchOutcome() {
+    if (playerScore === computerScore) {
+        matchOutcome = 'its a tie match';
+    } else if (playerScore > computerScore) {
+        matchOutcome = 'you won the match';
+    } else {
+        matchOutcome = 'you lost the match';
     }
-    // else the player's choice is compared with the computer's choice and the winner is determined
-    else {
-        switch (playerSelection) {
-            case "rock":
-                if (computerSelectionCleaned === "paper") {
-                    results.innerText = `${outcome[0] + winLose[0]}`;
-                    return LOSE;
-                } else if (computerSelectionCleaned === "scissors") {
-                    results.innerText = `${outcome[1] + winLose[1]}`;
-                    return WIN;
-                }
-                break;
-            case "paper":
-                if (computerSelectionCleaned === "scissors") {
-                    results.innerText = `${outcome[2] + winLose[0]}`;
-                    return LOSE;
-                } else if (computerSelectionCleaned === "rock") {
-                    results.innerText = `${outcome[0] + winLose[1]}`;
-                    return WIN;
-                }
-                break;
-            case "scissors":
-                if (computerSelectionCleaned === "rock") {
-                    results.innerText = `${outcome[1] + winLose[0]}`;
-                    return LOSE;
-                } else if (computerSelectionCleaned === "paper") {
-                    results.innerText = `${outcome[2] + winLose[1]}`;
-                    return WIN;
-
-                }
-                break;
-            default:
-                results.innerText = `Invalid Choice`;
-                console.log("Invalid Choice");
-                return DRAW;
-        }
-    }
-
+    updateFinalResults();
 }
-
 
 function game(playerSelection) {
-
-    let matchResult = 0;
-    let outcomeMessage = '';
-    const score = document.querySelector('#score');
-    const replay = document.querySelector('#replay');
-    const results = document.querySelector('#results');
-    const playerChoices = document.querySelector('#playerChoices');
-
-
-    console.log("Rock, Paper, Scissors!\n5 rounds total! Begin!");
-
-    if (rounds <= 5) {
-        score.innerText = `Round  ${rounds}!
-        \nPlayer Score:  ${playerScore}
-        \nComputer Score: ${computerScore}
-        \nRock, paper, or scissors?`
-
-        console.log("Round " + rounds + "!" +
-            "\nPlayer Score: " + playerScore +
-            "\nComputer Score: " + computerScore +
-            "\nRock, paper, or scissors?");
-        matchResult = playRound(playerSelection, computerPlay());
-        if (matchResult === WIN) {
-            playerScore++;
-        } else if (matchResult === LOSE) {
-            computerScore++;
-        } 
+    if (rounds > 5) {
+        updateMatchOutcome();
+        return
+    } else if (rounds === 5) {
+        playRound(playerSelection, computerPlay());
+        updateRoundOutcome();
         rounds++;
-    } else {
-        if (playerScore > computerScore) {
-            outcomeMessage = 'You Win!';
-        } else {
-            outcomeMessage = 'You Lose!';
-        }
-
-        results.classList.toggle('block');
-
-        //Final outcome message
-        score.innerText =
-            `\nPlayer Score:  ${playerScore}
-        \nComputer Score: ${computerScore}
-        \n${outcomeMessage}`;
-
-        replay.addEventListener('click', () => {
-            playerScore = 0;
-            computerScore = 0;
-            rounds = 1;
-            results.classList.toggle('block');
-            score.innerText = '';
-            results.innerText = '';   
-        });
-
     }
-
-    //Determining the final outcome message
+    else {
+        playRound(playerSelection, computerPlay());
+        updateRoundOutcome();
+        rounds++;
+    }
 
 }
 
+function replay() {
+    playerScore = 0;
+    computerScore = 0;
+    rounds = 1;
+}
 
-const playerChoices = document.querySelector('#playerChoices');
-const choiceBtns = playerChoices.querySelectorAll('button');
-let playerChoice = '';
+// USER INTERFACE
+
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const replayBtn = document.getElementById('replayBtn');
+const score = document.getElementById('score');
 
 
-choiceBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-        game(buttonSelection(button));
-        
-    });
-});
+rockBtn.addEventListener('click', () => game('ROCK'));
+paperBtn.addEventListener('click', () => game('PAPER'));
+scissorsBtn.addEventListener('click', () => game('SCISSORS'));
+
+replayBtn.addEventListener('click', () => replay());
+
+function updateScore() {
+    score.innerText = `Round  ${rounds}!
+        \nPlayer Score:  ${playerScore}
+        \nComputer Score: ${computerScore}
+        \nRock, paper, or scissors?`;
+}
+
+function updateFinalResults() {
+    score.innerText = `${matchOutcome}!
+        \nPlayer Score:  ${playerScore}
+        \nComputer Score: ${computerScore}
+        `;
+}
